f0f6dcbbd76925b28b397ecfa90b461a48489835
sunny <indiasuny000@gmail.com>
2014-01-07 15:35:28 +0530

    Icon added. Ready for release.

--- data/panel.js
+++ data/panel.js
@@ -1,5 +1,16 @@
-var show = document.getElementById('do');
+const showClass = 'show-class';
+const clearClass = 'clear-class';
+
+// Emit signal to show classes.
+var show = document.getElementById('show');
 show.onclick = function() {
   var el = document.getElementById('classname');
-  self.port.emit('show-class', el.value);
+  self.port.emit(showClass, el.value);
+}
+
+// Emit signal to hide classes.
+var clear = document.getElementById('clear');
+clear.onclick = function() {
+  self.port.emit(clearClass);
+  document.getElementById('classname').value = "";
 }
