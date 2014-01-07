16917ddd0593800ec1d3c76d9f4b038fda6d409b
sunny <indiasuny000@gmail.com>
2014-01-07 11:33:59 +0530

    Auto removeworking.

--- data/now.js
+++ data/now.js
@@ -1,11 +1,11 @@
-self.port.on('showw', function(name, prev) {
+self.port.on('showw', function(name) {
   elems = document.getElementsByClassName('same-Class-yolo');
-  console.log("count: " + elems.length);
-  console.log(JSON.stringify(elems));
-  for (var i=0; i<elems.length; i++) {
+  var e = Array.prototype.slice.call(elems);
+  for (var el of e) {
     console.log('removing..');
-    elems[i].classList.remove('same-Class-yolo');
+    el.classList.toggle('same-Class-yolo');
   }
+
   elems = document.getElementsByClassName(name);
   console.log("found " + elems.length);
   for (var ele of elems) {
