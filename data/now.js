83b912bd22db986980d3f04121da8f30fc2dfa7d
sunny <indiasuny000@gmail.com>
2014-01-07 08:38:02 +0530

    Class adding works. Removing problem.

--- data/now.js
+++ data/now.js
@@ -1,9 +1,14 @@
-self.port.on('showw', function(name) {
-  console.log("picked up!");
-  console.log("classname: " + name);
+self.port.on('showw', function(name, prev) {
+  elems = document.getElementsByClassName('same-Class-yolo');
+  console.log("count: " + elems.length);
+  console.log(JSON.stringify(elems));
+  for (var i=0; i<elems.length; i++) {
+    console.log('removing..');
+    elems[i].classList.remove('same-Class-yolo');
+  }
   elems = document.getElementsByClassName(name);
   console.log("found " + elems.length);
-  for (ele of elems) {
-    ele.style.border = '4px solid red';
+  for (var ele of elems) {
+    ele.classList.toggle('same-Class-yolo');
   }
 });
